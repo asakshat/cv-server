e20b966c1371d7268813ce16863c1ff960bb72ee
asakshat <asakshat453@gmail.com>
2024-05-16 16:07:13 +0200

    added login and signup

--- internal/routes/authRoutes.mjs
+++ internal/routes/authRoutes.mjs
@@ -1,7 +1,9 @@
 import express from 'express'
+import { loginUser ,signUpUser} from '../controllers/authController.js'
 const router = express.Router();
 
 
-router.post('/user/login',login)
+router.post('/user/login',loginUser)
+router.post('/user/signup',signUpUser)
 
 export default router
