b901f461eddad19571873f863ac96ba8b9f42e78
asakshat <asakshat453@gmail.com>
2024-05-23 16:29:17 +0200

    added User info functions

--- cmd/app.mjs
+++ cmd/app.mjs
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import { authenticateToken } from '../internal/middlewares/authenticate.mjs';
 import userAuth from '../internal/routes/authRoutes.mjs';
+import userInfo from '../internal/routes/userInfoRoutes.mjs';
 
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -38,6 +39,7 @@ app.get('/', (req, res) => {
 app.use('/api', userAuth);
 
 // anyhting after this will get token
+app.use('/api', userInfo);
 
 
 
--- internal/routes/authRoutes.mjs
+++ internal/routes/authRoutes.mjs
@@ -7,6 +7,5 @@ router.post('/user/login',loginUser)
 router.post('/user/signup',signUpUser)
 
 // user info 
-router.post('/user/info', userInfoHandler) 
 
 export default router
